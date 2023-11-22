41d896676c91b06dd2656f34d33aa5c62b3c9d35
Alan-Vasconi <alansouzeesi@@gmail.com>
2023-11-22 09:52:45 -0300

    Adicionar alterações problema 1 e 2

--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,48 @@ class Cliente {
   constructor(
     public nome: string,
     public cpf: string,
-    public tipoCarteira: string
+    public tipoCarteira: string,
+    public alugando: Veiculo | null = null
   ) {}
-}
 
+  estaAlugando(): boolean {
+    return this.alugando !== null;
+  }
+}
 class Veiculo {
   constructor(
     public placa: string,
     public modelo: string,
     public marca: string,
     public valorHoraAluguel: number,
-    public disponivel: boolean = true
+    public disponivel: boolean = true,
+    public usuario: Cliente | null = null,
+    public carteiraNecessario: string
   ) {}
 
-  alugar(): void {
-    if (this.disponivel) {
+  alugar(cliente: Cliente): void {
+    if (this.disponivel && !this.usuario && cliente.tipoCarteira === this.carteiraNecessario && !cliente.estaAlugando()) {
       this.disponivel = false;
-      console.log(`Veículo ${this.modelo} alugado com sucesso.`);
+      this.usuario = cliente;
+      cliente.alugando = this; // Atualiza o veículo alugado pelo cliente
+      console.log(`Veículo ${this.modelo} alugado por ${this.usuario.nome} com sucesso.`);
+    } else if (this.usuario) {
+      console.log(`Veículo ${this.modelo} está sendo usado por ${this.usuario.nome}.`);
+    } else if (cliente.estaAlugando()) {
+      console.log(`Cliente ${cliente.nome} já está alugando um veículo.`);
+    } else if (cliente.tipoCarteira !== this.carteiraNecessario) {
+      console.log(`Veículo ${this.modelo} requer uma carteira do tipo ${this.carteiraNecessario}.`);
     } else {
-      console.log(`Veículo ${this.modelo} não está disponível para aluguel.`);
+      console.log(`Veículo ${this.modelo} não está disponível para aluguel para este cliente.`);
     }
   }
 
   devolver(): void {
     if (!this.disponivel) {
       this.disponivel = true;
-      console.log(`Veículo ${this.modelo} devolvido com sucesso.`);
+      const usuarioNome = this.usuario ? this.usuario.nome : 'Desconhecido';
+      console.log(`Veículo ${this.modelo} devolvido por ${usuarioNome} com sucesso.`);
+      this.usuario = null;
     } else {
       console.log(`Veículo ${this.modelo} já está disponível.`);
     }
@@ -60,12 +76,18 @@ class Locadora {
     );
   }
 
-  alugarVeiculo(placa: string): void {
-     
-    const veiculoEncontrado = this.veiculos.find(veiculo => veiculo.placa === placa);
- 
+  alugarVeiculo(placa: string, cliente: Cliente): void { 
+    let veiculoEncontrado: Veiculo | undefined;
+
+    for (const veiculo of this.veiculos) {
+      if (veiculo.placa === placa) {
+        veiculoEncontrado = veiculo;
+        break;
+      }
+    }
+
     if (veiculoEncontrado) {
-      veiculoEncontrado.alugar();
+      veiculoEncontrado.alugar(cliente); 
     } else {
       console.log("Veículo não encontrado.");
     }
@@ -85,21 +107,27 @@ class Locadora {
 // Exemplo de uso //////////////////////////////////////////////////////
 const locadora = new Locadora();
 
-const carro1 = new Veiculo("PPX-5487", "Civic", "Honda", 10);
-const carro2 = new Veiculo("LLD-5391", "Corolla", "Toyota", 11);
-const carro3 = new Veiculo("YWK-1223", "Uno", "Fiat", 8);
-const carro4 = new Veiculo("PPX-5487", "Civic", "Honda", 10);
+const veiculo1 = new Veiculo("PPX-5487", "Civic", "Honda", 10, true, null, "B");
+const veiculo2 = new Veiculo("LLD-5391", "Corolla", "Toyota", 11, true, null, "B");
+const veiculo3 = new Veiculo("YWK-1223", "Uno", "Fiat", 8, true, null, "B");
+const veiculo4 = new Veiculo("OUTRA-PLACA", "Civic", "Honda", 10, true, null, "B");
+const veiculo5 = new Veiculo("BRA-49CC", "Sahara 300", "Honda", 5, true, null, "A");
 
-locadora.cadastrarVeiculo(carro1);
-locadora.cadastrarVeiculo(carro2);
-locadora.cadastrarVeiculo(carro3);
-locadora.cadastrarVeiculo(carro4);
+locadora.cadastrarVeiculo(veiculo1);
+locadora.cadastrarVeiculo(veiculo2);
+locadora.cadastrarVeiculo(veiculo3);
+locadora.cadastrarVeiculo(veiculo4);
+locadora.cadastrarVeiculo(veiculo5);
 
 locadora.listarVeiculosDisponiveis();
 
-locadora.alugarVeiculo("PPX-5487");
-locadora.alugarVeiculo("LLD-5391");
-locadora.alugarVeiculo("YWK-1223");
+const cliente1 = new Cliente("João", "12345678900", "A");
+const cliente2 = new Cliente("Maria", "98765432100", "B");
+
+locadora.alugarVeiculo("PPX-5487",cliente1);
+locadora.alugarVeiculo("LLD-5391",cliente2); 
+locadora.alugarVeiculo("YWK-1223",cliente1);
+
 
 locadora.listarVeiculosDisponiveis();
 
