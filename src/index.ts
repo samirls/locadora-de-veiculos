d0e1f232fb56531bd345cb3b334dc4cd24bbbf2b
Daniella <daniellamarialer@outlook.com>
2023-11-22 18:39:19 -0300

    Adiciona 1-2-3,1,2

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ class Cliente {
 
 class Veiculo {
   constructor(
+    public tipo: string,
     public placa: string,
     public modelo: string,
     public marca: string,
@@ -18,7 +19,9 @@ class Veiculo {
   alugar(): void {
     if (this.disponivel) {
       this.disponivel = false;
+
       console.log(`Veículo ${this.modelo} alugado com sucesso.`);
+
     } else {
       console.log(`Veículo ${this.modelo} não está disponível para aluguel.`);
     }
@@ -41,7 +44,6 @@ class Locadora {
   
     const veiculoExistente = this.veiculos.find((v) => v.placa === veiculo.placa);
 
-
     if (veiculoExistente) {
       console.log(`Veículo com a placa ${veiculo.placa} já está cadastrado.`);
     } else {
@@ -56,16 +58,17 @@ class Locadora {
     );
     console.log("Veículos disponíveis:");
     veiculosDisponiveis.forEach((veiculo) =>
-      console.log(`${veiculo.modelo} - ${veiculo.marca} - ${veiculo.placa}`)
+    console.log(`${veiculo.modelo} - ${veiculo.marca} - ${veiculo.placa}`)
     );
   }
 
   alugarVeiculo(placa: string): void {
      
     const veiculoEncontrado = this.veiculos.find(veiculo => veiculo.placa === placa);
- 
+
     if (veiculoEncontrado) {
       veiculoEncontrado.alugar();
+           
     } else {
       console.log("Veículo não encontrado.");
     }
@@ -85,15 +88,15 @@ class Locadora {
 // Exemplo de uso //////////////////////////////////////////////////////
 const locadora = new Locadora();
 
-const carro1 = new Veiculo("PPX-5487", "Civic", "Honda", 10);
-const carro2 = new Veiculo("LLD-5391", "Corolla", "Toyota", 11);
-const carro3 = new Veiculo("YWK-1223", "Uno", "Fiat", 8);
-const carro4 = new Veiculo("PPX-5487", "Civic", "Honda", 10);
+const carro1 = new Veiculo("Carro","PPX-5487", "Civic", "Honda", 10);
+const moto1 = new Veiculo("Moto","LLD-5391", "Corolla", "Toyota", 11);
+const carro3 = new Veiculo("Carro","YWK-1223", "Uno", "Fiat", 8);
+const moto2 = new Veiculo("Moto","PPX-5487", "Civic", "Honda", 10);
 
 locadora.cadastrarVeiculo(carro1);
-locadora.cadastrarVeiculo(carro2);
+locadora.cadastrarVeiculo(moto1);
 locadora.cadastrarVeiculo(carro3);
-locadora.cadastrarVeiculo(carro4);
+locadora.cadastrarVeiculo(moto2);
 
 locadora.listarVeiculosDisponiveis();
 
